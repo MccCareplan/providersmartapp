d03d1d55253f30ed5130b84cf11438c33ddee22b
Stephen Maier <smaier@cognitivemedicine.com>
2020-10-12 11:38:40 -0700

    Moved icon determination to component code

--- src/app/care-team/care-team.component.ts
+++ src/app/care-team/care-team.component.ts
@@ -7,13 +7,54 @@ import {DataService} from '../services/data.service';
   styleUrls: ['./care-team.component.css']
 })
 export class CareTeamComponent implements OnInit {
-  gridColumns = 4;
+
 
   constructor(public dataservice: DataService) {
   }
+  gridColumns = 4;
+
+  iconNames = {
+    types:
+      [
+        {type: 'person', icon: 'person'},
+        {type: 'organization', icon: 'group'}
+      ],
+    roles:
+      [
+        {role: 'patient', icon: 'sick'},
+        {role: 'catering', icon: 'food_bank'},
+        {role: 'physician', icon: 'medical_services'},
+        {role: 'nurse', icon: 'medical_services'},
+        {role: 'caregiver', icon: 'medical_services'},
+        {role: 'ologist', icon: 'medical_services'},
+        {role: 'dietician', icon: 'fastfood'},
+        {role: 'social worker', icon: 'psychology'},
+        {role: 'pharmacist', icon: 'medical_services'},
+      ]
+  };
 
   ngOnInit(): void {
 
+  }
+
+  getTypeIcon(type) {
+     const icon = this.iconNames.types.filter( t => type.toLowerCase().includes(t.type.toLowerCase()));
+     console.log('in getTypeIcon() type: ', type, 'icon: ', icon);
+     if (icon.length >  0) {
+       return icon[0].icon;
+     } else {
+       return '';
+     }
+    }
+
+  getRoleIcon(role) {
+    const icon = this.iconNames.roles.filter( r => role.toLowerCase().includes(r.role.toLowerCase()));
+    console.log('in getRoleIcon() role: ', role, 'icon: ', icon);
+    if (icon.length > 0) {
+      return icon[0].icon;
+    } else {
+      return '';
+    }
   }
 
 }
